f411a6459d3aecfacc6b44141e6b6ccc6f8d16fd
Ben Suen <ben1one@gmail.com>
2017-01-06 18:54:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,9 @@ app.post('/webhook/', function(req, res) {
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text.toLowerCase()
-									
+								
+			console.log(text)							
+								
 			if (event.message.hasOwnProperty("quick_reply")) {
 				let quick_reply_payload = event.message.quick_reply.payload
 				continue
